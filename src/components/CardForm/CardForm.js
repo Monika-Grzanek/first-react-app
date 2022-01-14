2d12ceb9006af3cebe126e24e4064cda386210db
Monika Grzanek <monski@o2.pl>
2022-01-14 14:38:07 +0100

    Add Card and CardForm in components and changes in Column.js, ColumnForm.js, List.js

---
+++ src/components/CardForm/CardForm.js
@@ -0,0 +1,21 @@
+import styles from './CardForm.module.scss';
+import { useState } from 'react';
+import Button from './../Button/Button.js';
+import TextInput from './../TextInput/TextInput.js';
+
+const CardForm = props => {
+    const [title, setTitle] = useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        props.action({title: title}, props.columnId);
+        setTitle('');
+    };
+    return (
+        <form className={styles.cardForm} onSubmit={handleSubmit}>
+            <TextInput type="text" value={title} onChange={e => setTitle(e.target.value)} />
+            <Button>Add card</Button>
+        </form>
+    );
+};
+
+export default CardForm;
