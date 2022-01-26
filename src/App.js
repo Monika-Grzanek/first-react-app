be1ab7fd8a4135449612da9e2db05810616d8c0d
Monika Grzanek <monski@o2.pl>
2022-01-26 13:14:00 +0100

    Add component NotFound and add Router in App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar/NavBar.js'
 import Home from './components/Home/Home.js'
 import Favorite from './components/Favorite/Favorite.js';
 import About from './components/About/About.js';
+import NotFound from './components/NotFound/NotFound.js';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/favorite" element={<Favorite />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </Container>
     </main>
